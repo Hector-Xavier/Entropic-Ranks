bdad04642f174696437f1967d84b01a0f26692b6
Hector - Xavier de Lastic <37729624+Hector-Xavier@users.noreply.github.com>
2018-04-24 15:31:03 +0300

    Update Entropic Ranks v1.0.R

--- Entropic Ranks v1.0.R
+++ Entropic Ranks v1.0.R
@@ -1,89 +1,120 @@
 require("RankProd")
+#reliable compatibility with RankProd versions up to 2.44.0
 require("entropy")
 require("factoextra")
 
-entropic_analysis <- function(ordered_vector,step_up=1,window_size,bins,verbose=FALSE)
+entropic_analysis <- function(ordered_vector,step_up=1,window_size,bins,verbose=FALSE,export_plots=FALSE,path=NULL)
 {
   differences <- ordered_vector[seq(2,length(ordered_vector))]-ordered_vector[seq(1,length(ordered_vector)-1)]
-  
   entropy_plotter <- vector(length=floor((length(differences)-window_size)/step_up))
   for (i in 0:(length(entropy_plotter)-1))
-  {
     entropy_plotter[i+1] <- entropy(discretize(differences[(i*step_up+1):(i*step_up+window_size)],numBins=bins),method="Laplace")
-  }
   
-  temp <- eclust(entropy_plotter, "kmeans", k=2, nstart=200, graph=FALSE)
-  
+  entropy_clusters <- eclust(entropy_plotter, "kmeans", k=2, nstart=200, graph=FALSE)
+
   if (verbose)
   {
-    message("Calculating entropies of ",length(entropy_plotter)," ovelapping windows")
-    message("Suggested cutoff at feature no ",seq(length(temp$cluster))[temp$cluster!=temp$cluster[1]][1]-1,", at a mean entropy of ",mean(entropy_plotter[1:seq(length(temp$cluster))[temp$cluster!=temp$cluster[1]][1]]))
-    message("Last 1/3 entropy infimum: ",min(entropy_plotter[floor(length(entropy_plotter)*2/3):length(entropy_plotter)]))
+    cat("Calculating entropies of ",length(entropy_plotter)," ovelapping windows","\n","Suggested cutoff at feature no ",seq(length(entropy_clusters$cluster))[entropy_clusters$cluster!=entropy_clusters$cluster[1]][1]-1,", at a mean entropy of ",mean(entropy_plotter[1:seq(length(entropy_clusters$cluster))[entropy_clusters$cluster!=entropy_clusters$cluster[1]][1]]),"\n","Last 1/3 minimum entropy: ",min(entropy_plotter[floor(length(entropy_plotter)*2/3):length(entropy_plotter)]),"\n",sep="")
     barplot(entropy_plotter,xlab=c("Granularity = ",step_up),ylab=c("Window size = ",window_size),main=c(bins," bins"),names.arg=seq(length(entropy_plotter))*step_up)
-    barplot(temp$silinfo$widths$sil_width,ylim=c(-0.3,1),ylab="Silhouette width",xlab="Clustered elements",main="K-means clustering quality")
+    barplot(entropy_clusters$silinfo$widths$sil_width,ylim=c(-0.3,1),ylab="Silhouette width",xlab="Clustered elements",main="K-means clustering quality")
+  }
+  if (export_plots)
+  {
+    if (is.null(path))
+      path <- paste(getwd(),"Entropic Ranks plots",sep="/")
+    if(!file.exists(path))
+      dir.create(path)
+    png(file=file.path(path,paste("Entropy - ",bins," bins - ",window_size," window size",".png", sep = "")))
+    barplot(entropy_plotter,xlab=c("Granularity = ",step_up),ylab=c("Window size = ",window_size),main=c(bins," bins"),names.arg=seq(length(entropy_plotter))*step_up)
+    dev.off()
+    png(file=file.path(path,paste("Clustering quality - ",bins," bins - ",window_size," window size",".png", sep = "")))
+    barplot(entropy_clusters$silinfo$widths$sil_width,ylim=c(-0.3,1),ylab="Silhouette width",xlab="Clustered elements",main="K-means clustering quality")
+    dev.off()
   }
   
-  return((seq(length(temp$cluster))[temp$cluster!=temp$cluster[1]][1]-1)*step_up)
+  return((seq(length(entropy_clusters$cluster))[entropy_clusters$cluster!=entropy_clusters$cluster[1]][1]-1)*step_up)
 }
 
-isolate_significant_elements <- function(data_under_analysis,granularity=1,process_log=FALSE)
+isolate_significant_elements <- function(ordered_vector,granularity=1,supervised=FALSE,process_log=FALSE,export_plots=FALSE,path=NULL)
 {
   suggested_surfaces <- c()
   
   if (process_log)
-    par(mfrow=c(2,1))
-  
-  for (i in 1:5)
   {
+    par(mfrow=c(2,1))
+  } else {
+    progress <- txtProgressBar(max=80,char="=",style=3,title="Entropic analysis progress",label="testing")
+  }
+  for (i in 1:5)
     for (j in 5:20)
     {
       if (process_log)
       {
-        message("Reiterating for ",i*5," bins and a sliding window of ",j*10," features")
+        cat("Reiterating for ",i*5," bins and a sliding window of ",j*10," features","\n",sep="")
+      } else {
+        setTxtProgressBar(progress,(i-1)*16+j-4)
       }
-      suggested_surfaces <- c(suggested_surfaces,entropic_analysis(ordered_vector=data_under_analysis,step_up=granularity,bins=i*5,window_size=j*10,verbose=process_log))
+      suggested_surfaces <- c(suggested_surfaces,entropic_analysis(ordered_vector,step_up=granularity,bins=i*5,window_size=j*10,verbose=process_log,export_plots,path))
     }
+  if (process_log)
+  {
+    par(mfrow=c(1,1))
+  } else {
+    close(progress)
   }
   
-  if (process_log)
+  if (supervised||process_log)
   {
     print(table(suggested_surfaces))
     message("Most consistent cutoff point: feature no ",as.integer(rownames(table(suggested_surfaces))[table(suggested_surfaces) == max(table(suggested_surfaces))])[1],".")
-    par(mfrow=c(1,1))
   }
-  return(as.integer(rownames(table(suggested_surfaces))[table(suggested_surfaces) == max(table(suggested_surfaces))])[1])
+  if (!supervised)
+    return(as.integer(rownames(table(suggested_surfaces))[table(suggested_surfaces) == max(table(suggested_surfaces))])[1])
 }
 
-entropic_ranks <- function(data_under_analysis,population_vector,data_origin=NULL,granularity=1,process_log=FALSE,create_output_files=FALSE,is_logged=TRUE,logbase=2,huge_feature_list=FALSE)
+entropic_ranks <- function(data_under_analysis,population_vector,data_origin=NULL,granularity=1,supervised=FALSE,process_log=FALSE,export_plots=FALSE,create_output_files=FALSE,is_logged=TRUE,logbase=2,huge_feature_list=FALSE)
 {
   if (is.null(data_origin))
-  {
     data_origin <- rep(1,length(population_vector))
-  }
   
+  message("Calculating Rank Products. May take a lot of time, depending on data size.")
   comparison <- RPadvance(data_under_analysis,cl=population_vector,origin=data_origin,logged=is_logged,na.rm=FALSE,gene.names=rownames(data_under_analysis),plot=process_log,huge=TRUE)
 
-  if (huge_feature_list)
-  {
-    rank_product_lists <- topGene(comparison,method="pfp",num.gene=5000,logged=is_logged,logbase=logbase,gene.names=rownames(data_under_analysis))
+  if (huge_feature_list){
+    message("Investigating only the first 5000 features.")
+    rank_product_lists <- topGene(comparison,num.gene=5000,logged=is_logged,logbase=logbase,gene.names=rownames(data_under_analysis))
   }else{
-    rank_product_lists <- topGene(comparison,cutoff=0.99,method="pfp",num.gene=NULL,logged=is_logged,logbase=logbase,gene.names=rownames(data_under_analysis))
+    rank_product_lists <- topGene(comparison,cutoff=0.99,method="pfp",logged=is_logged,logbase=logbase,gene.names=rownames(data_under_analysis))
   }
   
-  rank_product_lists$Table1 <- rank_product_lists$Table1[1:isolate_significant_elements(rank_product_lists$Table1[,2],granularity,process_log),]
-  rank_product_lists$Table2 <- rank_product_lists$Table2[1:isolate_significant_elements(rank_product_lists$Table2[,2],granularity,process_log),]
-  
-  if (create_output_files)
+  if (export_plots)
   {
-    if (dim(rank_product_lists$Table1)[1]>0)
-    {
-      write.table(file="Downregulated list.txt",rank_product_lists$Table1[,c(3:5)],sep="\t",quote=FALSE,row.names=TRUE,col.names=TRUE)
-    }
+    if(!file.exists(paste(getwd(),"Entropic Ranks plots",sep="/")))
+      dir.create(paste(getwd(),"Entropic Ranks plots",sep="/"))
+    path_down <- paste(getwd(),"Entropic Ranks plots","Downregulated",sep="/")
+    path_up <- paste(getwd(),"Entropic Ranks plots","Upregulated",sep="/")
+  }
+  
+  if (!supervised)
+  {
+    message("Trimming down the list of downregulated features without supervision...")
+    rank_product_lists$Table1 <- rank_product_lists$Table1[1:isolate_significant_elements(rank_product_lists$Table1[,2],granularity,supervised,process_log,export_plots,path=path_down),]
+    message("Trimming down the list of upregulated features without supervision...")
+    rank_product_lists$Table2 <- rank_product_lists$Table2[1:isolate_significant_elements(rank_product_lists$Table2[,2],granularity,supervised,process_log,export_plots,path=path_up),]
     
-    if (dim(rank_product_lists$Table2)[1]>0)
+    if (create_output_files)
     {
-      write.table(file="Upregulated list.txt",rank_product_lists$Table2[,3:5],sep="\t",quote=FALSE,row.names=TRUE,col.names=TRUE)
+      if (dim(rank_product_lists$Table1)[1]>0)
+        write.table(file="Downregulated list.txt",rank_product_lists$Table1[,3:5],sep="\t",quote=FALSE,row.names=TRUE,col.names=TRUE)
+      if (dim(rank_product_lists$Table2)[1]>0)
+        write.table(file="Upregulated list.txt",rank_product_lists$Table2[,3:5],sep="\t",quote=FALSE,row.names=TRUE,col.names=TRUE)
+      message("Output files created successfully.")
     }
+  } else {
+    message("Calculating suggested cutoff points for the downregulated feature list...")
+    isolate_significant_elements(rank_product_lists$Table1[,2],granularity,supervised,process_log,export_plots,path=path_down)
+    message("Calculating suggested cutoff points for the upregulated feature list...")
+    isolate_significant_elements(rank_product_lists$Table2[,2],granularity,supervised,process_log,export_plots,path=path_up)
   }
   return(rank_product_lists)
 }

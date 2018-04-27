f7c13bb2fafa53362647f6d8bd70b487916e52cc
Hector - Xavier de Lastic <37729624+Hector-Xavier@users.noreply.github.com>
2018-04-27 12:57:30 +0300

    Update Entropic Ranks v1.0.R

--- Entropic Ranks v1.0.R
+++ Entropic Ranks v1.0.R
@@ -9,14 +9,13 @@ entropic_analysis <- function(ordered_vector,step_up=1,window_size,bins,verbose=
   entropy_plotter <- vector(length=floor((length(differences)-window_size)/step_up))
   for (i in 0:(length(entropy_plotter)-1))
     entropy_plotter[i+1] <- entropy(discretize(differences[(i*step_up+1):(i*step_up+window_size)],numBins=bins),method="Laplace")
-  
   entropy_clusters <- eclust(entropy_plotter, "kmeans", k=2, nstart=200, graph=FALSE)
-
+  
   if (verbose)
   {
     cat("Calculating entropies of ",length(entropy_plotter)," ovelapping windows","\n","Suggested cutoff at feature no ",seq(length(entropy_clusters$cluster))[entropy_clusters$cluster!=entropy_clusters$cluster[1]][1]-1,", at a mean entropy of ",mean(entropy_plotter[1:seq(length(entropy_clusters$cluster))[entropy_clusters$cluster!=entropy_clusters$cluster[1]][1]]),"\n","Last 1/3 minimum entropy: ",min(entropy_plotter[floor(length(entropy_plotter)*2/3):length(entropy_plotter)]),"\n",sep="")
-    barplot(entropy_plotter,xlab=c("Granularity = ",step_up),ylab=c("Window size = ",window_size),main=c(bins," bins"),names.arg=seq(length(entropy_plotter))*step_up)
-    barplot(entropy_clusters$silinfo$widths$sil_width,ylim=c(-0.3,1),ylab="Silhouette width",xlab="Clustered elements",main="K-means clustering quality")
+    barplot(entropy_plotter,border=c("gold1","dodgerblue3")[as.vector(as.integer(entropy_clusters$cluster==entropy_clusters$cluster[1])+1)],col=c("gold1","dodgerblue3")[as.vector(as.integer(entropy_clusters$cluster==entropy_clusters$cluster[1])+1)],xlab=c("Granularity = ",step_up),ylab=c("Window size = ",window_size),main=c(bins," bins"),names.arg=seq(length(entropy_plotter))*step_up)
+    barplot(entropy_clusters$silinfo$widths$sil_width,border=c("gold1","dodgerblue3")[as.integer(entropy_clusters$silinfo$widths$cluster==entropy_clusters$cluster[1])+1],col=c("gold1","dodgerblue3")[as.integer(entropy_clusters$silinfo$widths$cluster==entropy_clusters$cluster[1])+1],ylim=c(-0.3,1),ylab="Silhouette width",xlab="Clustered elements",main="K-means clustering quality")
   }
   if (export_plots)
   {
@@ -24,11 +23,11 @@ entropic_analysis <- function(ordered_vector,step_up=1,window_size,bins,verbose=
       path <- paste(getwd(),"Entropic Ranks plots",sep="/")
     if(!file.exists(path))
       dir.create(path)
-    png(file=file.path(path,paste("Entropy - ",bins," bins - ",window_size," window size",".png", sep = "")))
-    barplot(entropy_plotter,xlab=c("Granularity = ",step_up),ylab=c("Window size = ",window_size),main=c(bins," bins"),names.arg=seq(length(entropy_plotter))*step_up)
+    png(file=file.path(path,paste("Entropy - ",bins," bins - ",window_size," window size",".png", sep = "")),width=640,height=640)
+    barplot(entropy_plotter,border=c("gold1","dodgerblue3")[as.vector(as.integer(entropy_clusters$cluster==entropy_clusters$cluster[1])+1)],col=c("gold1","dodgerblue3")[as.vector(as.integer(entropy_clusters$cluster==entropy_clusters$cluster[1])+1)],xlab=c("Granularity = ",step_up),ylab=c("Window size = ",window_size),main=c(bins," bins"),names.arg=seq(length(entropy_plotter))*step_up)
     dev.off()
-    png(file=file.path(path,paste("Clustering quality - ",bins," bins - ",window_size," window size",".png", sep = "")))
-    barplot(entropy_clusters$silinfo$widths$sil_width,ylim=c(-0.3,1),ylab="Silhouette width",xlab="Clustered elements",main="K-means clustering quality")
+    png(file=file.path(path,paste("Clustering quality - ",bins," bins - ",window_size," window size",".png", sep = "")),width=640,height=640)
+    barplot(entropy_clusters$silinfo$widths$sil_width,border=c("gold1","dodgerblue3")[as.integer(entropy_clusters$silinfo$widths$cluster==entropy_clusters$cluster[1])+1],col=c("gold1","dodgerblue3")[as.integer(entropy_clusters$silinfo$widths$cluster==entropy_clusters$cluster[1])+1],ylim=c(-0.3,1),ylab="Silhouette width",xlab="Clustered elements",main="K-means clustering quality")
     dev.off()
   }
   
@@ -38,7 +37,6 @@ entropic_analysis <- function(ordered_vector,step_up=1,window_size,bins,verbose=
 isolate_significant_elements <- function(ordered_vector,granularity=1,supervised=FALSE,process_log=FALSE,export_plots=FALSE,path=NULL)
 {
   suggested_surfaces <- c()
-  
   if (process_log)
   {
     par(mfrow=c(2,1))
@@ -80,7 +78,6 @@ entropic_ranks <- function(data_under_analysis,population_vector,data_origin=NUL
   
   message("Calculating Rank Products. May take a lot of time, depending on data size.")
   comparison <- RPadvance(data_under_analysis,cl=population_vector,origin=data_origin,logged=is_logged,na.rm=FALSE,gene.names=rownames(data_under_analysis),plot=process_log,huge=TRUE)
-
   if (huge_feature_list){
     message("Investigating only the first 5000 features.")
     rank_product_lists <- topGene(comparison,num.gene=5000,logged=is_logged,logbase=logbase,gene.names=rownames(data_under_analysis))
@@ -95,7 +92,6 @@ entropic_ranks <- function(data_under_analysis,population_vector,data_origin=NUL
     path_down <- paste(getwd(),"Entropic Ranks plots","Downregulated",sep="/")
     path_up <- paste(getwd(),"Entropic Ranks plots","Upregulated",sep="/")
   }
-  
   if (!supervised)
   {
     message("Trimming down the list of downregulated features without supervision...")
